18f872e6fa321abc64f810c2f10a7eb7a7a42a78
wikoci <74468439+wikoci@users.noreply.github.com>
2021-08-08 15:54:30 +0200

    graphql add

---
+++ test.js
@@ -0,0 +1,15 @@
+const Strapi = require("./index");
+
+const strapi = new Strapi({
+  baseURL: "http://127.0.0.1:1337",
+  baseURLgraphql: "http://127.0.0.1:1337/graphql",
+  debug: true,
+});
+
+var query = `
+{patients{nom}}
+`;
+
+strapi.graphql({
+  query: query,
+});
